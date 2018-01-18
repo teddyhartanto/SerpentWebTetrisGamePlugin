aee691615db1fae7a37794ef2cd8514264f57877
Teddy Hartanto <hartantoteddy@yahoo.com>
2018-01-18 14:20:07 +0800

    Initial commit with game plugin to open Tetris Battle on FB using Google Chrome

---
+++ files/serpent_TetrisBattleFB_game.py
@@ -0,0 +1,48 @@
+from serpent.game import Game
+
+from .api.api import TetrisBattleFBAPI
+
+from serpent.utilities import Singleton
+
+from serpent.game_launchers.web_browser_game_launcher import WebBrowser
+
+
+class SerpentTetrisBattleFBGame(Game, metaclass=Singleton):
+
+    def __init__(self, **kwargs):
+        kwargs["platform"] = "web_browser"
+        kwargs["browser"] = WebBrowser.CHROME
+        kwargs["window_name"] = "Tetris Battle on Facebook - Google Chrome"
+        kwargs["url"] = "https://apps.facebook.com/tetris_battle/"
+
+        super().__init__(**kwargs)
+
+        self.api_class = TetrisBattleFBAPI
+        self.api_instance = None
+
+    @property
+    def screen_regions(self):
+        regions = {
+            "SAMPLE_REGION": (0, 0, 0, 0)
+        }
+
+        return regions
+
+    @property
+    def ocr_presets(self):
+        presets = {
+            "SAMPLE_PRESET": {
+                "extract": {
+                    "gradient_size": 1,
+                    "closing_size": 1
+                },
+                "perform": {
+                    "scale": 10,
+                    "order": 1,
+                    "horizontal_closing": 1,
+                    "vertical_closing": 1
+                }
+            }
+        }
+
+        return presets
